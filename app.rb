a47b990b381b8073d6f7c2beb5b73a33b6b9786b
Miki <miki@vaadin.com>
2016-04-14 11:06:49 +0300

    step 10 - grid is lazy loading

--- app.rb
+++ app.rb
@@ -7,10 +7,20 @@ enable :sessions
 helpers Vaadin::ViewHelpers
 
 before do
+  Encoding.default_external=Encoding::UTF_8
   @cards, @sets = %w{AllCards SetList}.collect { |filename| JSON.parse(File.read("#{filename}.json")) }
   @types = %w{Artifact Creature Enchantment Instant Land Sorcery Planeswalker}
   # cmcs actually need to contain some string, because of https://github.com/vaadin-miki/vaadin-elements/issues/10
-  @cmcs = @cards.values.collect { |v| v['cmc'] }.uniq.compact.sort.collect { |x| "CMC #{x}" }
+  @cmcs = @cards.values.collect { |v| v['cmc'] }.uniq.compact.sort.collect { |x| "#{x}" }
+end
+
+post '/cards' do
+  # from cards, select
+  matching = @cards.select { |name, data| !name.include?("token card") && session[:filters].all? { |type, values| values.empty? || data[type].nil? || (data[type].is_a?(Array) && values.any? { |v| data[type].include?(v) }) || values.any? { |v| data[type].to_s == v.to_s } } }
+  puts "Selecting #{params['count']} cards from #{matching.size} matching the current filters, starting at index #{params['index']}."
+
+  content_type 'application/json', charset: 'utf-8'
+  {result: matching.values[params['index'].to_i, params['count'].to_i], size: matching.size}.to_json
 end
 
 post '/update' do
@@ -27,15 +37,13 @@ post '/update' do
   if params["value"].empty? then
     session[:filters][attribute].clear
   elsif !session[:filters][attribute].include?(params['value'])
-    session[:filters][attribute] << (attribute == 'cmc' ? params['value'][4..-1] : params['value'])
+    session[:filters][attribute] << params['value']
   end
 
-  # from cards, select
-  matching = @cards.select { |name, data| !name.include?("token card") && session[:filters].all? { |type, values| values.empty? || data[type].nil? || (data[type].is_a?(Array) && values.any? { |v| data[type].include?(v) }) || values.any? { |v| data[type].to_s == v.to_s } } }
-  puts "Current filters: #{session[:filters].inspect} would select #{matching.size} cards."
+  puts "Current filters: #{session[:filters].inspect}."
 
   content_type 'application/json', charset: 'utf-8'
-  {grid: {items: matching.values}, size: {innerHTML: matching.size}, active: {innerHTML: session[:filters].inspect}}.to_json
+  {grid: {refreshItems: nil}, active: {innerHTML: session[:filters].inspect}}.to_json
 end
 
 get '/' do
